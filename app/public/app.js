1049fdf1eee48f8a8c0bc05a407caea50788b79c
Roberto Alvarez <robertoatc02@gmail.com>
2014-10-17 21:28:22 -0700

    Initialize app

---
+++ app/public/app.js
@@ -0,0 +1,26 @@
+var app,
+  testJSON;
+
+testJSON = [
+  {
+    user: 'drinki',
+    quote: 'estaria cool que le supieras a la vida',
+    description: 'dicho a wo despues de opinar acerca de x'
+  },
+  {
+    user: 'without',
+    quote: 'me caga bruno mars pero canta como doscientosunmil angeles y jesus es su padre'
+  }
+];
+
+app = angular.module('bukiquotes', []);
+
+// app.config(function ($routeProvider) {
+//   $routeProvider.when('/', {
+//     templateUrl: 
+//   })
+// })
+
+app.controller('bukiquotesCtrl', ['$scope', function ($scope) {
+
+}]);
