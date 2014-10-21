2f7295af23917a7bc2837b8889366d8778af9dfe
Daniel Almaguer <danielalmaguer@gmail.com>
2014-10-20 19:02:38 -0700

    Added Login Functions

--- app/public/app.js
+++ app/public/app.js
@@ -1,49 +1,49 @@
 angular
-    .module('bukiquotes', [
-        'PostDirective',
-        'API',
-        'satellizer'
-    ])
+  .module('bukiquotes', [
+    'PostDirective',
+    'API',
+    'satellizer'
+  ])
 
-    .config(function($authProvider) {
-      $authProvider.facebook({
-        clientId: '295545623927393',
-        url: 'http://localhost:1337/auth/facebook'
-      });
-    })
+  .config(function($authProvider) {
+    $authProvider.facebook({
+      clientId: '295545623927393',
+      url: 'http://localhost:1337/auth/facebook'
+    });
+  })
 
-    .controller('bukiCtrl', ['$scope', '$auth', '$http', 'APIService', function($scope, $auth, $http, APIService) {
+  .controller('bukiCtrl', ['$scope', '$auth', '$http', 'APIService', function($scope, $auth, $http, APIService) {
 
-      $scope.authenticate = function(provider) {
-        $auth.authenticate(provider);
-      };
+    $scope.authenticate = function(provider) {
+      $auth.authenticate(provider);
+    };
 
-      $scope.logout = function() {
-          $auth.logout();
-      };
+    $scope.logout = function() {
+      $auth.logout();
+    };
 
-      $scope.me = function() {
-        $http.get('http://localhost:1337/api/me')
-          .then(function(data) {
-              console.log(data.data);
-          })
-          .catch(function(err) {
-            console.log(err.data);
-          })
-      };
+    $scope.me = function() {
+      $http.get('http://localhost:1337/api/me')
+        .then(function(data) {
+          console.log(data.data);
+        })
+        .catch(function(err) {
+          console.log(err.data);
+        })
+    };
 
-        console.log($auth.isAuthenticated());
+    console.log($auth.isAuthenticated());
 
-      $scope.posts = [
-        {
-          username: 'drinki',
-          quote: 'estaria cool que le supieras a la vida',
-          desc: 'dicho a wo despues de opinar acerca de x'
-        },
-        {
-          username: 'without',
-          quote: 'me caga bruno mars pero canta como doscientosunmil angeles y jesus es su padre'
-        }
-      ];
+    $scope.posts = [
+      {
+        username: 'drinki',
+        quote: 'estaria cool que le supieras a la vida',
+        desc: 'dicho a wo despues de opinar acerca de x'
+      },
+      {
+        username: 'without',
+        quote: 'me caga bruno mars pero canta como doscientosunmil angeles y jesus es su padre'
+      }
+    ];
 
-    }]);
+  }]);
