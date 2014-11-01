53fb626eed1789d2520aef69deee7c4c303f97af
Daniel Almaguer <danielalmaguer@gmail.com>
2014-11-01 13:03:40 -0700

    Initial mysql

--- api/server.js
+++ api/server.js
@@ -4,10 +4,64 @@ var express = require('express'),
     logger = require('morgan'),
     cors = require('cors'),
     config = require('./config'),
-    db = require('./helpers/db_helper'),
-    routes = require('./routes/routes');
+    //db = require('./helpers/db_helper'),
+    Sequelize = require('sequelize');
+    //routes = require('./routes/routes');
 
-db.initialize();
+//db.initialize();
+
+
+
+
+var sequelize = new Sequelize('seqtest', 'root', null);
+
+var User = sequelize.define('User', {
+    username: Sequelize.STRING,
+    email: Sequelize.STRING,
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    fullName: Sequelize.STRING,
+    facebookId: Sequelize.STRING
+}, {
+    timestamps: false
+});
+
+var Quote = sequelize.define('Quote', {
+    text: {
+        type: Sequelize.STRING,
+        validate: { notEmpty: true }
+    },
+    context: Sequelize.STRING,
+    score: Sequelize.DECIMAL(10, 2)
+}, {
+    timestamps: true
+});
+
+Quote.belongsTo(User, { as: 'Author', foreignKey: 'authorId' });
+Quote.belongsTo(User, { as: 'Poster', foreignKey: 'posterId' });
+
+
+// Creating tables
+sequelize.sync({ force: true }) // Emit success or failure
+    .success(function() {
+        User.create({
+            username: 'deini',
+            email: 'danielalmaguer@gmail.com',
+            firstName: 'Daniel',
+            lastName: 'Almaguer',
+            fullName: 'Daniel Almaguer',
+            facebookId: '100003234521485'
+        })
+            .success(function(user) {
+                Quote.create({
+                    text: 'Da Dream'
+                })
+                    .success(function(quote) {
+                        quote.setAuthor(user);
+                    });
+            });
+    });
+
 
 // Vars
 var app = express(),
@@ -20,8 +74,9 @@ app
     .use(logger('dev'))
     .use(cors({ origin: origin }));
 
-routes(app);
+//routes(app);
 
 app.listen(app.get('port'), function() {
     console.log('Server running on port ' + app.get('port'));
 });
+
