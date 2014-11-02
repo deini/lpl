db2f2db48eb5ae19e7b71d6b688e0f68b10db0d6
Daniel Almaguer <danielalmaguer@gmail.com>
2014-11-01 21:07:33 -0700

    Changed db name

--- api/db/db.js
+++ api/db/db.js
@@ -2,6 +2,6 @@
 var config = require('./../config'),
     Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('seqtest', 'root', null);
+var sequelize = new Sequelize('bukiquotes', 'root', null);
 
 module.exports = sequelize;
